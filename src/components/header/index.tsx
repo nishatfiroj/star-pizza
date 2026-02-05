513ece3bf07bfc52239f38733a899a5fe4978fd1
Nishat Firoj <nishat.firoj@gmail.com>
2026-02-04 22:08:14 -0500

    set menu button for mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import menu from "@/public/menu-icon.svg"
 import Image from "next/image"
 import { useBreakpoints, useCurrentPage } from "@/src/utils/hooks"
 import { Pages } from "@/src/utils/types"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 import { StyledNavBarItem } from "./styled"
 
 export function Header() {
@@ -53,6 +53,8 @@ function NavBarItem({ page, children }: { page: Pages; children?: ReactNode }) {
   )
 }
 function MobileHeader() {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
     <header>
       <Grid
@@ -62,11 +64,47 @@ function MobileHeader() {
         justifyContent="space-between"
       >
         <Grid>
-          <Image height={56} src={logo} alt={"Star Pizza"} />
+          <NavBarItem page={"home"}>
+            <Image height={72} src={logo} alt={"Star Pizza"} />
+          </NavBarItem>
         </Grid>
-        <Grid p={2}>
+        <Grid p={2} onClick={() => setOpenMenu(true)}>
           <Image src={menu} width={24} height={24} alt={"Open menu"} />
         </Grid>
+        {openMenu && (
+          <Grid
+            style={{
+              position: "absolute",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "flex-start",
+              padding: "1em",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              zIndex: 10000,
+              backgroundColor: "var(--secondary-color)",
+            }}
+          >
+            <Grid container flexDirection="column">
+              <NavBarItem page={"menu"} />
+              <NavBarItem page={"catering"} />
+              <NavBarItem page={"coupons"} />
+              <NavBarItem page={"delivery"} />
+              <NavBarItem page={"contact"} />
+            </Grid>
+
+            <Grid
+              style={{ position: "absolute", right: 24 }}
+              onClick={() => setOpenMenu(false)}
+            >
+              <p>X</p>
+            </Grid>
+          </Grid>
+        )}
       </Grid>
     </header>
   )
