87c4bf5f3370cf71b7b4b729d9047a1a6dc75138
Nishat Firoj <nishat.firoj@gmail.com>
2026-02-08 16:13:51 -0500

    reference env vars in vercel

--- api/getMenu.ts
+++ api/getMenu.ts
@@ -6,13 +6,13 @@ import keys from "../spreadsheet-keys.json"
 export async function getMenu() {
   try {
     const auth = await google.auth.getClient({
-      projectId: keys.project_id,
+      projectId: process.env.project_id ?? keys.project_id,
       credentials: {
         type: "service_account",
-        private_key: keys.private_key,
-        client_email: keys.client_email,
-        client_id: keys.client_id,
-        token_url: keys.token_uri,
+        private_key: process.env.private_key ?? keys.private_key,
+        client_email: process.env.client_email ?? keys.client_email,
+        client_id: process.env.client_id ?? keys.client_id,
+        token_url: process.env.token_uri ?? keys.token_uri,
         universe_domain: "googleapis.com",
       },
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
